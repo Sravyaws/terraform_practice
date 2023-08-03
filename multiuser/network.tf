64510b68ac75690f76992fbfdf9ff11f31534da5
Sravyaws <Sravyaguduru18@gmail.com>
2023-08-03 09:14:32 +0530

    added multiuser

--- multiuser/network.tf
+++ multiuser/network.tf
@@ -3,5 +3,10 @@ resource "aws_vpc" "ntier" {
     tags = {
       Name = "ntier"
     }
-
+}
+resource "aws_vpc" "ntier" {
+    cidr_block = "192.168.0.0/16"
+    tags = {
+      Name = "backend"
+    }
 }
